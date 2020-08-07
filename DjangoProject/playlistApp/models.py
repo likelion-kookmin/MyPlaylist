641c61b145325a5310770dee2ea5a5fdfd0e2fef
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2020-08-08 03:43:57 +0900

    playlist 모델링 멤버함수 추가

--- DjangoProject/playlistApp/models.py
+++ DjangoProject/playlistApp/models.py
@@ -13,9 +13,22 @@ class Playlist(models.Model):
     putIn = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='playlist_putIn', blank = True) #플레이리스트를 담은 유저
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name= 'playlist_wirter') #플레이리스트의 작성자
 
-    #플레이리스트 제목 반환
+    #플레이리스트 정보 반환
     def __str__(self):
-        return self.title
+        return f"{self.title} / 총{len(self.get_songs())}곡 / 좋아요: {self.get_num_of_likes()} / 담기: {self.get_num_of_putIn()} / 조회수: {self.look}"
+
+    #플레이리스트 좋아요수 반환
+    def get_num_of_likes(self):
+        return self.like.all().count()
+
+    #플레이리스트 담기수 반환
+    def get_num_of_putIn(self):
+        return self.putIn.all().count()
+
+    #플레이리스트 내의 Song객체의 리스트 반환
+    def get_songs(self):
+        return [i.songObj for i in Relationship.objects.filter(playlistObj=self)]
+
 
 #각각 곡의 모델
 class Song(models.Model):
@@ -30,11 +43,15 @@ class Song(models.Model):
     floUid = models.CharField(max_length = 200) #flo에서의 곡 id - flo에서만 id가 string으로 저장됨.
     viveUid = models.IntegerField() #vive에서의 곡 id
 
+    #곡 정보 반환
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.artist}"
 
 #플레이리스트와 노래를 연결해주는 모델
 class Relationship(models.Model):
     index = models.IntegerField() #플레이리스트 내의 곡 순서
-    playlistObj = models.ManyToManyField(Playlist, related_name = 'relate_to_playlist') #연결된 플레이리스트
-    songObj = models.ManyToManyField(Song, related_name= 'relate_to_song') #연결된 곡
+    playlistObj = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name = 'relate_to_playlist') #연결된 플레이리스트
+    songObj = models.ForeignKey(Song, on_delete=models.CASCADE, related_name= 'relate_to_song') #연결된 곡
+
+    def __str__(self):
+        return f"{self.playlistObj.title} [{self.index}번째 {self.songObj}]"
